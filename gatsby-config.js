ad871a590803b814328eecb926bcdb67754a43a3
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-06-28 19:05:16 +0700

    up

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const config = require("./data/SiteConfig");
 
 module.exports = {
   siteMetadata: {
-    title: 'Câu Chuyện Bí Mật',
+    title: 'Lời Người Xưa',
     siteUrl: config.siteUrl,
     rssMetadata: {
       site_url: config.siteUrl ,
@@ -83,7 +83,7 @@ module.exports = {
         setup(ref) {
           const ret = ref.query.site.siteMetadata.rssMetadata;
           ret.allMarkdownRemark = ref.query.allMarkdownRemark;
-          ret.generator = "Câu Chuyện Bí Mật";
+          ret.generator = "Lời Người Xưa";
           return ret;
         },
         query: `
