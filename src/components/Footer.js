fb619b6fe01cd369522bba01a230df68ffcda6df
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-07-02 21:15:51 +0700

    up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () =>
     <footer className="site-footer outer">
         <div className="site-footer-content inner">
             <section className="copyright">
-                <Link to="/">iloves.live</Link> | Lời Người Xưa
+                <Link to="/">suonerie.org</Link> | Lời Người Xưa
             </section>
             <nav>
                 <a className="social-logo" href="https://youtu.be/N4WTi9EHLqA" >
