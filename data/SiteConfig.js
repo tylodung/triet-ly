a4df71eb8974bc26718e43781fedc24ee3d32d01
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-07-02 21:14:50 +0700

    up

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
     siteTitle: "Lời Người Xưa", // Site title.
     siteTitleAlt: "Chuyện mới kể..", // Alternative site title for SEO.
     siteLogo: "https://api.adorable.io/avatars/150/logo.png", // Logo used for SEO and manifest.
-    siteUrl: "https://iloves.live/", // Domain of your website without pathPrefix.
+    siteUrl: "https://suonerie.org/", // Domain of your website without pathPrefix.
     pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
     siteDescription: "Nhóm của Bình.", // Website description used for RSS feeds/meta description tag.
     siteRss: "/rss.xml", // Path to the RSS file.
